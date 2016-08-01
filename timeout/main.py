fb80c09f0509c2806938dfecc69705d3085fec92
Celine <celine.choo98@gmail.com>
2016-08-01 19:47:47 -0400

    changes to main.py, added dashboard files

--- timeout/main.py
+++ timeout/main.py
@@ -7,22 +7,39 @@ jinja_environment = jinja2.Environment(loader =
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 # ================== HANDLERS ====================
-# actual handlers
+""" HANDLER INFORMATION
+    url: /
+    handler: IndexHandler
+    frontend: /templates/index.html,            /stylesheets/index.css     """
 class IndexHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/index.html')
         self.response.write(template.render())
 
+""" HANDLER INFORMATION
+    url: /response
+    handler: ResponseHandler
+    frontend: /templates/response.html,         /stylesheets/response.css
+              /templates/response_confirm.html, /stylesheets/repsonse.css   """
 class ResponseHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/response.html')
-        #added_points = int(self.request.get('points'))
-        self.response.write(template.render())
+        # added_points = int(self.request.get('points'))
+        # this is where you would query and fetch a list of all compliments, then get a random item from that list.
+        temp = {
+            "compliment": "value to display in html"
+        }
+        self.response.write(template.render(temp))
 
     def post(self):
         template = jinja_environment.get_template('templates/response_confirm.html')
         self.response.write(template.render())
 
+""" HANDLER INFORMATION
+    url: /write
+    handler: WriteHandler
+    frontend: /templates/write.html,            /stylesheets/write.css
+              /templates/write_confirm.html     /stylesheets/write.css      """
 class WriteHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('templates/write.html')
@@ -30,9 +47,20 @@ class WriteHandler(webapp2.RequestHandler):
 
     def post(self):
         template = jinja_environment.get_template('templates/write_confirm.html')
+        self.response.write(template.render())
         new_compliment = self.request.get('words')
-        complimentObj = Compliment(content=new_compliment,points=0,views=0)
+        complimentObj = Compliment(content=new_compliment,points=4,views=0)
+        complimentObj.addPoints(3)
         comp_key = complimentObj.put()
+
+""" HANDLER INFORMATION
+    url: /dashboard
+    handler: DashHandler
+    frontend: /templates/dashboard.html,        /stylesheets/dashboard.css     """
+class DashHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/dashboard.html')
+        self.response.write(template.render())
 
 # ================ OBJECTS =================
 # user object, for each login. ONLY instantiated when a person logs in with gmail username.
@@ -48,8 +76,14 @@ class Compliment(ndb.Model):
     points = ndb.IntegerProperty(required=True)
     views = ndb.IntegerProperty(required=True)
 
+    def addPoints(self,inc):
+        self.points += inc
+
+
+# ============== LINKS ===============
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
     ('/response', ResponseHandler),
-    ('/write', WriteHandler)
+    ('/write', WriteHandler),
+    ('/dashboard', DashHandler)
 ], debug=True)
