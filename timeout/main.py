8938cc185c0d35e484523f91c542e35b7da61ed1
Celine <celine.choo98@gmail.com>
2016-08-01 15:25:46 -0400

    edits to main.py file --celine

--- timeout/main.py
+++ timeout/main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import os
 import jinja2
@@ -22,11 +6,15 @@ from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(loader =
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-# HANDLERS!!!!
-
+# ================== HANDLERS ===================
+# actual handlers
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+class FeelingHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/index.html')
+        self.response.write(template.render())
 
 # test handlers
 class CelineTestHandler(webapp2.RequestHandler):
@@ -42,17 +30,19 @@ class PeteTestHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('templates/write.html')
         self.response.write(template.render())
 
-class FeelingHandler(webapp2.RequestHandler):
-
+# ================ OBJECTS =================
+# user object, for each login. ONLY instantiated when a person logs in with gmail username.
 class User(ndb.Model):
-    name=ndb.StringProperty(required=True)
-    email=ndb.StringProperty(required=True)
-    compliment_list= ndb.KeyProperty(kind="Compliment_list", repeated=True)
+    name = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    compliment_list = ndb.KeyProperty('Compliment', repeated=True)
 
+# compliment object, created every time someone WRITES a compliment.
+# called every time someone ASKS FOR a compliment.
 class Compliment(ndb.Model):
-    string=ndb.StringProperty(required=True)
-    points=ndb.StringProperty(required=True)
-    views=ndb.IntegerProperty(required=True)
+    content = ndb.StringProperty(required=True)
+    points = ndb.IntegerProperty(required=True)
+    views = ndb.IntegerProperty(required=True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
