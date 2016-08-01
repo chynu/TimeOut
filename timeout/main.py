aedf0f165df840640b5037381897be59418ef8cb
Celine <celine.choo98@gmail.com>
2016-08-01 12:59:57 -0400

    added pete test handler

--- timeout/main.py
+++ timeout/main.py
@@ -22,38 +22,41 @@ from google.appengine.ext import ndb
 jinja_environment = jinja2.Environment(loader =
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+# HANDLERS!!!!
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
-# class User(ndb.Model):
-#     name=ndb.StringProperty(required=True)
-#     email=ndb.StringProperty(required=True)
-#     compliment_list= ndb.KeyProperty(kind="Compliment_list", repeated=True)
+# test handlers
+class CelineTestHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/index.html')
+        self.response.write(template.render())
+class NigelTestHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/response.html')
+        self.response.write(template.render())
+class PeteTestHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('templates/write.html')
+        self.response.write(template.render())
+
+class FeelingHandler(webapp2.RequestHandler):
+
+class User(ndb.Model):
+    name=ndb.StringProperty(required=True)
+    email=ndb.StringProperty(required=True)
+    compliment_list= ndb.KeyProperty(kind="Compliment_list", repeated=True)
 
 class Compliment(ndb.Model):
     string=ndb.StringProperty(required=True)
     points=ndb.StringProperty(required=True)
     views=ndb.IntegerProperty(required=True)
 
-
-
-
-
-
-
-class CelineTestHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('templates/index.html')
-        self.response.write(template.render())
-
-class NigelTestHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_environment.get_template('templates/response.html')
-        self.response.write(template.render())
-
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/celix', CelineTestHandler),
-    ('/nigel', NigelTestHandler)
+    ('/nigel', NigelTestHandler),
+    ('/pete', PeteTestHandler)
 ], debug=True)
