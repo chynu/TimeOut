c3f0dea2521418e3b4623206f050b13848f91366
celine <celine.choo98@gmail.com>
2016-08-04 14:44:19 -0400

    updates

---
+++ timeout/scripts/index.js
@@ -0,0 +1,8 @@
+var quest = $("#quest");
+var popup = $(".popup");
+var clicked = false;
+quest.click(function(){
+  if (clicked){ popup.fadeOut(300); }
+  else { popup.fadeIn(300); }
+  clicked = !clicked;
+});
